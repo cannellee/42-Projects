f8b002d08c8b570dc278862e776b4c9767a127d7
Cannelle <142885920+cannellee@users.noreply.github.com>
2024-03-25 20:41:29 +0100

    Create ft_boolean.h

---
+++ piscines/ft_boolean.h
@@ -0,0 +1,14 @@
+//NOTE : RUN WITH day_21_060_062.c !!
+#ifndef FT_BOOLEAN_H_
+#define FT_BOOLEAN_H_
+
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define SUCCESS 0
+#define EVEN(x) (x%2==0)
+typedef int t_bool;
+
+
+#endif
