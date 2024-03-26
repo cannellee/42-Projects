d10156a2b10ae90571df2ece18958f37548ecc34
Cannelle <142885920+cannellee@users.noreply.github.com>
2024-03-26 18:10:11 +0100

    Create ft_stock_par.h

---
+++ piscines/ft_stock_par.h
@@ -0,0 +1,12 @@
+//NOTE : RUN WITH day_22_063_065.c !!
+#ifndef FT_STOCK_PAR_H_
+#define FT_STOCK_PAR_H_
+
+typedef struct s_stock_par {
+	int size_param;
+	char *str;
+	char *copy;
+	char **tab;
+} t_stock_par;
+
+#endif
