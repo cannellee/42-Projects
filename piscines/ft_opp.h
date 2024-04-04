e554d703a296b889ac332b011b5b1cac1594ca93
Cannelle <142885920+cannellee@users.noreply.github.com>
2024-04-04 21:16:26 +0200

    Create ft_opp.h

---
+++ piscines/ft_opp.h
@@ -0,0 +1,12 @@
+//NOTE : RUN WITH day_30_087_089.c !!
+#ifndef __FT_OPP_H__
+#define __FT_OPP_H__
+
+t_opp gl_opptab[]={
+	{"+", &ft_add},
+	{"-", &ft_minus},
+	{"*", &ft_mul},
+	{"/", &ft_div},
+	{"%", &ft_mod},
+	{"", &ft_usage}};
+#endif
